5431b6f32ba965a7c6cf941e00f78b7a14821941
Jeffrey <darkthread@gmail.com>
2023-05-11 21:41:40 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace prevent_shutdown;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        components = new System.ComponentModel.Container();
+        btnPunchOut = new Button();
+        lblTime = new Label();
+        lblPunchOutTime = new Label();
+        timer1 = new System.Windows.Forms.Timer(components);
+        cbxAuto = new CheckBox();
+        statusStrip = new StatusStrip();
+        lblStatus = new ToolStripStatusLabel();
+        statusStrip.SuspendLayout();
+        SuspendLayout();
+        // 
+        // btnPunchOut
+        // 
+        btnPunchOut.Font = new Font("Microsoft JhengHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+        btnPunchOut.Location = new Point(100, 6);
+        btnPunchOut.Name = "btnPunchOut";
+        btnPunchOut.Size = new Size(88, 34);
+        btnPunchOut.TabIndex = 0;
+        btnPunchOut.Text = "下班打卡";
+        btnPunchOut.UseVisualStyleBackColor = true;
+        btnPunchOut.Click += btnPunchOut_Click;
+        // 
+        // lblTime
+        // 
+        lblTime.AutoSize = true;
+        lblTime.BackColor = Color.FromArgb(64, 64, 64);
+        lblTime.Font = new Font("Microsoft JhengHei UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+        lblTime.ForeColor = Color.Lime;
+        lblTime.Location = new Point(12, 11);
+        lblTime.Name = "lblTime";
+        lblTime.Size = new Size(84, 24);
+        lblTime.TabIndex = 1;
+        lblTime.Text = "00:00:00";
+        lblTime.DoubleClick += lblTime_DoubleClick;
+        // 
+        // lblPunchOutTime
+        // 
+        lblPunchOutTime.AutoSize = true;
+        lblPunchOutTime.Font = new Font("Microsoft JhengHei UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+        lblPunchOutTime.Location = new Point(104, 37);
+        lblPunchOutTime.Name = "lblPunchOutTime";
+        lblPunchOutTime.Size = new Size(0, 19);
+        lblPunchOutTime.TabIndex = 2;
+        // 
+        // timer1
+        // 
+        timer1.Enabled = true;
+        timer1.Interval = 1000;
+        timer1.Tick += timer1_Tick;
+        // 
+        // cbxAuto
+        // 
+        cbxAuto.AutoSize = true;
+        cbxAuto.Location = new Point(194, 16);
+        cbxAuto.Name = "cbxAuto";
+        cbxAuto.Size = new Size(98, 19);
+        cbxAuto.TabIndex = 3;
+        cbxAuto.Text = "登出自動打卡";
+        cbxAuto.UseVisualStyleBackColor = true;
+        // 
+        // statusStrip
+        // 
+        statusStrip.Items.AddRange(new ToolStripItem[] { lblStatus });
+        statusStrip.Location = new Point(0, 44);
+        statusStrip.Name = "statusStrip";
+        statusStrip.Size = new Size(292, 24);
+        statusStrip.TabIndex = 4;
+        statusStrip.Text = "statusStrip1";
+        // 
+        // lblStatus
+        // 
+        lblStatus.Font = new Font("Microsoft JhengHei UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+        lblStatus.Name = "lblStatus";
+        lblStatus.Size = new Size(99, 19);
+        lblStatus.Text = "尚未打下班卡";
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(292, 68);
+        Controls.Add(statusStrip);
+        Controls.Add(cbxAuto);
+        Controls.Add(lblPunchOutTime);
+        Controls.Add(lblTime);
+        Controls.Add(btnPunchOut);
+        FormBorderStyle = FormBorderStyle.Fixed3D;
+        Name = "Form1";
+        Text = "模擬打卡程式";
+        statusStrip.ResumeLayout(false);
+        statusStrip.PerformLayout();
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    #endregion
+
+    private Button btnPunchOut;
+    private Label lblTime;
+    private Label lblPunchOutTime;
+    private System.Windows.Forms.Timer timer1;
+    private CheckBox cbxAuto;
+    private StatusStrip statusStrip;
+    private ToolStripStatusLabel lblStatus;
+}
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System.Runtime.InteropServices;
+
+namespace prevent_shutdown;
+//https://www.meziantou.net/prevent-windows-shutdown-or-session-ending-in-dotnet.htm
+public partial class Form1 : Form
+{
+    public const int WM_QUERYENDSESSION = 0x0011;
+    public const int WM_ENDSESSION = 0x0016;
+    public const uint SHUTDOWN_NORETRY = 0x00000001;
+
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern bool ShutdownBlockReasonCreate(IntPtr hWnd, [MarshalAs(UnmanagedType.LPWStr)] string reason);
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern bool ShutdownBlockReasonDestroy(IntPtr hWnd);
+    [DllImport("kernel32.dll")]
+    static extern bool SetProcessShutdownParameters(uint dwLevel, uint dwFlags);
+    public Form1()
+    {
+        InitializeComponent();
+        // Define the priority of the application (0x3FF = The higher priority)
+        SetProcessShutdownParameters(0x3FF, SHUTDOWN_NORETRY);
+        timer1_Tick(null!, null!);
+    }
+
+    protected override void WndProc(ref Message m)
+    {
+        if (m.Msg == WM_QUERYENDSESSION || m.Msg == WM_ENDSESSION)
+        {
+            if (!ReadyForShutdown()) return;
+        }
+        base.WndProc(ref m);
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+        lblTime.Text = DateTime.Now.ToString("HH:mm:ss");
+    }
+
+    CancellationTokenSource _cts = null;
+    private int _countDown = -1;
+    private bool InCountDown => _countDown > -1;
+    private bool _done = false;
+
+    private bool ReadyForShutdown()
+    {
+        if (_done) return true;
+        if (InCountDown) return false;
+        // Prevent windows shutdown
+        ShutdownBlockReasonCreate(this.Handle, "休蛋幾壘，不打下班卡逆？" +
+                             (cbxAuto.Checked ? "自動打卡中..." : string.Empty));
+        if (!cbxAuto.Checked) return false;
+        StartCountDown();
+        Task.Run(async () =>
+        {
+            while (_countDown-- > 0 && !_cts.Token.IsCancellationRequested)
+            {
+                this.BeginInvoke(UpdatePunchOutBtn);
+                await Task.Delay(1000);
+            }
+            if (_cts.Token.IsCancellationRequested) return;
+            this.BeginInvoke(() =>
+            {
+                PunchOut();
+                ShutdownBlockReasonCreate(this.Handle, "自動打卡完成。");
+                ShutdownBlockReasonDestroy(this.Handle);
+                this.Close();
+            });
+        }, _cts.Token);
+        return false;
+    }
+
+    void StartCountDown()
+    {
+        _cts = new CancellationTokenSource();
+        lblStatus.Text = @"自動打卡中...";
+        _countDown = 15;
+        UpdatePunchOutBtn();
+    }
+    void StopCountDown()
+    {
+        _cts.Cancel();
+        lblStatus.Text = @"自動打卡已取消";
+        _countDown = -1;
+        UpdatePunchOutBtn();
+    }
+    void UpdatePunchOutBtn()
+    {
+        btnPunchOut.Text = _countDown == -1 ? "打卡下班" : $"取消({_countDown})";
+    }
+
+    void PunchOut()
+    {
+        lblStatus.ForeColor = Color.Green;
+        lblStatus.Text = $@"於{DateTime.Now:HH:mm:ss}打卡下班";
+        _done = true;
+    }
+
+    private void btnPunchOut_Click(object sender, EventArgs e)
+    {
+        if (InCountDown) StopCountDown(); else PunchOut();
+    }
+
+    // 模疑觸發關機事件，方便測試
+    private void lblTime_DoubleClick(object sender, EventArgs e)
+        => ReadyForShutdown();
+}
